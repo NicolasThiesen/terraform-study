c58771595947f31110299eb9670404c63c5e4785
Nicolas Thiesen <nicolasnmth@gmail.com>
2026-03-08 18:20:22 +0000

    update vnet name

--- azure-vnet/main.tf
+++ azure-vnet/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "resource_group" {
 
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "example-network"
+  name                = "vnet"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
   address_space       = ["10.0.0.0/16"]
